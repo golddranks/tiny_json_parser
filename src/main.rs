9ed125e9e98689a66e744e6ebaa585e7662a55a9
Pyry Kontio <pyry.kontio@drasa.eu>
2022-10-30 17:02:04 +0900

    Implement Objects

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tiny_json_parser::{parse, Val, Error, Number, Array};
+use tiny_json_parser::{parse, Error, Val, key};
 
 fn main() {
     let json0 = br#""#;
@@ -6,6 +6,7 @@ fn main() {
     let json2 = br#"true"#;
     let json3 = br#"false"#;
     let json4 = br#"1.0"#;
+    let json13 = br#""test""#;
     let json5 = br#"[]"#;
     let json6 = br#"[null]"#;
     let json7 = br#"[null, true]"#;
@@ -13,111 +14,161 @@ fn main() {
     let json9 = br#"[null, [], false]"#;
     let json10 = br#"[null, [true], false]"#;
     let json11 = br#"[null, [true, true], false]"#;
+    let json12 = br#"{"a": null, "b": {"c": true, "d": null}, "e": false}"#;
 
-    let mut p = parse(json0).unwrap();
+    let mut p = parse(json0);
     assert_eq!(p.val(), Err(Error));
 
-    let mut p = parse(json1).unwrap();
-    assert_eq!(p.val().unwrap(), Val::Null);
+    let mut p = parse(json1);
+    assert_eq!(p.val(), Ok(Val::Null));
 
-    let mut p = parse(json2).unwrap();
+    let mut p = parse(json2);
     assert_eq!(p.val().unwrap(), Val::Boolean(true));
 
-    let mut p = parse(json3).unwrap();
+    let mut p = parse(json3);
     assert_eq!(p.val().unwrap(), Val::Boolean(false));
 
-    let mut p = parse(json4).unwrap();
+    let mut p = parse(json4);
     if let Val::Number(num) = p.val().unwrap() {
         assert_eq!(num.as_bytes(), b"1.0");
+        assert_eq!(num.as_str(), "1.0");
     } else {
         panic!();
     }
 
-    match parse(json5).unwrap().val().unwrap() {
+    let mut p = parse(json13);
+    if let Val::String(str) = p.val().unwrap() {
+        assert_eq!(str.as_str(), "test");
+    } else {
+        panic!();
+    }
+
+    match parse(json5).val().unwrap() {
         Val::Array(mut a) => {
             assert_eq!(a.next().unwrap(), None);
-        },
+        }
         _ => panic!(),
     };
 
-    match parse(json6).unwrap().val().unwrap() {
+    match parse(json6).val().unwrap() {
         Val::Array(mut a) => {
-            assert_eq!(a.next().unwrap().unwrap(), Val::Null);
+            assert_eq!(a.next().unwrap(), Some(Val::Null));
             assert_eq!(a.next().unwrap(), None);
-        },
+        }
         _ => panic!(),
     };
 
-
-    match parse(json7).unwrap().val().unwrap() {
+    match parse(json7).val().unwrap() {
         Val::Array(mut a) => {
-            assert_eq!(a.next().unwrap().unwrap(), Val::Null);
-            assert_eq!(a.next().unwrap().unwrap(), Val::Boolean(true));
+            assert_eq!(a.next().unwrap(), Some(Val::Null));
+            assert_eq!(a.next().unwrap(), Some(Val::Boolean(true)));
             assert_eq!(a.next().unwrap(), None);
-        },
+        }
         _ => panic!(),
     };
 
-    match parse(json8).unwrap().val().unwrap() {
-        Val::Array(mut a) => {
-            assert_eq!(a.next().unwrap().unwrap(), Val::Null);
-            assert_eq!(a.next().unwrap().unwrap(), Val::Boolean(true));
-            assert_eq!(a.next().unwrap().unwrap(), Val::Boolean(false));
+    match parse(json8).val() {
+        Ok(Val::Array(mut a)) => {
+            assert_eq!(a.next().unwrap(), Some(Val::Null));
+            assert_eq!(a.next().unwrap(), Some(Val::Boolean(true)));
+            assert_eq!(a.next().unwrap(), Some(Val::Boolean(false)));
             assert_eq!(a.next().unwrap(), None);
-        },
+        }
         _ => panic!(),
     };
 
-    match parse(json9).unwrap().val().unwrap() {
-        Val::Array(mut a) => {
-            assert_eq!(a.next().unwrap().unwrap(), Val::Null);
-            if let Val::Array(mut a) = a.next().unwrap().unwrap() {
-                assert_eq!(a.next().unwrap(), None);
+    match parse(json9).val() {
+        Ok(Val::Array(mut a)) => {
+            assert_eq!(a.next().unwrap(), Some(Val::Null));
+            if let Ok(Some(Val::Array(mut b))) = a.next() {
+                assert_eq!(b.next().unwrap(), None);
             } else {
                 panic!();
             };
-            assert_eq!(a.next().unwrap().unwrap(), Val::Boolean(false));
+            assert_eq!(a.next().unwrap(), Some(Val::Boolean(false)));
             assert_eq!(a.next().unwrap(), None);
-        },
+        }
         _ => panic!(),
     };
 
-    match parse(json9).unwrap().val().unwrap() {
-        Val::Array(mut a) => {
-            assert_eq!(a.next().unwrap().unwrap(), Val::Null);
-            let Val::Array(_) = a.next().unwrap().unwrap() else {
+    match parse(json9).val() {
+        Ok(Val::Array(mut a)) => {
+            assert_eq!(a.next().unwrap(), Some(Val::Null));
+            let Ok(Some(Val::Array(_))) = a.next() else {
                 panic!();
             };
-            assert_eq!(a.next().unwrap().unwrap(), Val::Boolean(false));
+            assert_eq!(a.next().unwrap(), Some(Val::Boolean(false)));
             assert_eq!(a.next().unwrap(), None);
-        },
+        }
         _ => panic!(),
     };
 
-    match parse(json10).unwrap().val().unwrap() {
-        Val::Array(mut a) => {
-            assert_eq!(a.next().unwrap().unwrap(), Val::Null);
-            if let Val::Array(mut a) = a.next().unwrap().unwrap() {
-                assert_eq!(a.next().unwrap().unwrap(), Val::Boolean(true));
-                assert_eq!(a.next().unwrap(), None);
+    match parse(json10).val() {
+        Ok(Val::Array(mut a)) => {
+            assert_eq!(a.next().unwrap(), Some(Val::Null));
+            if let Ok(Some(Val::Array(mut b))) = a.next() {
+                assert_eq!(b.next().unwrap(), Some(Val::Boolean(true)));
+                assert_eq!(b.next().unwrap(), None);
             } else {
                 panic!();
             };
-            assert_eq!(a.next().unwrap().unwrap(), Val::Boolean(false));
+            assert_eq!(a.next().unwrap(), Some(Val::Boolean(false)));
             assert_eq!(a.next().unwrap(), None);
-        },
+        }
         _ => panic!(),
     };
 
-    match parse(json10).unwrap().val().unwrap() {
-        Val::Array(mut a) => {
-            assert_eq!(a.next().unwrap().unwrap(), Val::Null);
-            let Val::Array(_) = a.next().unwrap().unwrap() else {
+    match parse(json10).val() {
+        Ok(Val::Array(mut a)) => {
+            assert_eq!(a.next().unwrap(), Some(Val::Null));
+            let Ok(Some(Val::Array(_))) = a.next() else {
                 panic!();
             };
-            assert_eq!(a.next().unwrap().unwrap(), Val::Boolean(false));
+            assert_eq!(a.next().unwrap(), Some(Val::Boolean(false)));
             assert_eq!(a.next().unwrap(), None);
-        },
+        }
+        _ => panic!(),
+    };
+
+    match parse(json11).val() {
+        Ok(Val::Array(mut a)) => {
+            assert_eq!(a.next().unwrap(), Some(Val::Null));
+            if let Ok(Some(Val::Array(mut b))) = a.next() {
+                assert_eq!(b.next().unwrap(), Some(Val::Boolean(true)));
+                assert_eq!(b.next().unwrap(), Some(Val::Boolean(true)));
+                assert_eq!(b.next().unwrap(), None);
+            } else {
+                panic!();
+            };
+            assert_eq!(a.next().unwrap(), Some(Val::Boolean(false)));
+        }
+        _ => panic!(),
+    };
+
+    match parse(json11).val() {
+        Ok(Val::Array(mut a)) => {
+            assert_eq!(a.next(), Ok(Some(Val::Null)));
+            let Ok(Some(Val::Array(_))) = a.next() else {
+                panic!();
+            };
+            assert_eq!(a.next(), Ok(Some(Val::Boolean(false))));
+        }
+        _ => panic!(),
+    };
+
+    match parse(json12).val() {
+        Ok(Val::Object(mut o)) => {
+            assert_eq!(o.next(), Ok(Some((key("a"), Val::Null))));
+            if let Ok(Some((k, Val::Object(mut p)))) = o.next() {
+                assert_eq!(k, key("b"));
+                assert_eq!(p.next().unwrap(), Some((key("c"), Val::Boolean(true))));
+                assert_eq!(p.next().unwrap(), Some((key("d"), Val::Null)));
+                assert_eq!(p.next().unwrap(), None);
+            } else {
+                panic!();
+            };
+            assert_eq!(o.next(), Ok(Some((key("e"), Val::Boolean(false)))));
+        }
         _ => panic!(),
     };
 }
